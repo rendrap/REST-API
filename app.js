96dc246f2bf5e7fd0604908cf1b50efc0f207f47
rendrap <rendrapradana@gmail.com>
2019-09-04 12:45:08 +0700

    adding body-parser

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
+const db = mongoose.connect('mongodb://localhost/bookAPI');
+const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
+
+bookRouter.route('/books')
+  .post((req, res) => {
+    const book = new Book(req.body);
+  })
+  .get((req, res) => {
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
+  });
+
+bookRouter.route('/books/:bookId')
+  .get((req, res) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(book);
+    });
+  });
+
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome to my new API');
